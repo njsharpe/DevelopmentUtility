f9b10f4caa5c1dc1d3054cfd80463040a1d8a493
njsharpe <nelsonj.sharpe@gmail.com>
2022-03-14 17:23:14 -0400

    (fix) make WorldHelper methods visible

--- src/main/java/net/njsharpe/developmentutility/helper/WorldHelper.java
+++ src/main/java/net/njsharpe/developmentutility/helper/WorldHelper.java
@@ -10,11 +10,11 @@ import org.jetbrains.annotations.Nullable;
 public class WorldHelper {
 
     @Contract("_,null -> false")
-    private boolean canSeeSkyFromBelowWater(@NotNull World world, @Nullable Location location) {
+    public static boolean canSeeSkyFromBelowWater(@NotNull World world, @Nullable Location location) {
         if(location == null) return false;
-        if(location.getY() >= world.getSeaLevel()) return this.canSeeSky(world, location);
+        if(location.getY() >= world.getSeaLevel()) return WorldHelper.canSeeSky(world, location);
         Location pos = new Location(world, location.getX(), world.getSeaLevel(), location.getZ());
-        if(!this.canSeeSky(world, pos)) return false;
+        if(!WorldHelper.canSeeSky(world, pos)) return false;
         pos = pos.add(0, -1, 0);
         while(pos.getY() > location.getY()) {
             BlockState state = world.getBlockState(pos);
@@ -25,7 +25,7 @@ public class WorldHelper {
     }
 
     @Contract("_,null -> false")
-    private boolean canSeeSky(@NotNull World world, @Nullable Location location) {
+    public static boolean canSeeSky(@NotNull World world, @Nullable Location location) {
         if(location == null) return false;
         return world.hasSkyLight() && location.getBlock().getLightFromSky() >= 15;
     }
